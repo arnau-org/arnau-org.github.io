72df51314ac0b084db644c7b64ef67ea5a68cd1a
arnau-org <71732803+arnau-org@users.noreply.github.com>
2024-09-30 17:09:12 +0200

    Create alpine.sh

---
+++ alpine.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+apk add parted xfsprogs
+
+sleep 5
+
+parted --script /dev/sda mklabel gpt
+parted --script --align=optimal /dev/sda mkpart ESP fat32 1MiB 4GiB
+parted --script --align=optimal /dev/sda mkpart primary 4GiB 100%
+parted --script /dev/sda set 1 boot on
+
+sleep 5
+
+mkfs.vfat /dev/sda1
+mkfs.xfs /dev/sda2
+
+sleep 5
+
+wget https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/alpine-extended-3.20.3-x86_64.iso
+
+sleep 5
+
+mount -t vfat /dev/sda1 /mnt
+cd /mnt
+uniso < /root/alpine-extended-3.20.2-x86_64.iso
+
+sleep 5
+
+sync
+
+sleep 5
+
+cd ~ && umount /mnt

c3839d0f17b2797653571d5b6cdf29cdf8e0f2d7
arnau-org <71732803+arnau-org@users.noreply.github.com>
2024-12-12 17:53:57 +0100

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -3,16 +3,16 @@
 apk add parted efibootmgr
 
 parted --script /dev/sda mklabel gpt
-parted --script --align=optimal /dev/sda mkpart ESP fat32 1MiB 4GiB
-parted --script --align=optimal /dev/sda mkpart primary 4GiB 100%
+parted --script --align=optimal /dev/sda mkpart ESP fat32 1MiB 2GiB
+parted --script --align=optimal /dev/sda mkpart primary 2GiB 100%
 parted --script /dev/sda set 1 boot on
 
 mkfs.vfat /dev/sda1
 
-if wget https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/alpine-extended-3.20.3-x86_64.iso; then
+if wget https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/x86_64/alpine-extended-3.21.0-x86_64.iso; then
   mount -t vfat /dev/sda1 /mnt
   cd /mnt
-  uniso < /root/alpine-extended-3.20.3-x86_64.iso
+  uniso < /root/alpine-extended-3.21.0-x86_64.iso
   sync
   cd ~ && umount /mnt
   efibootmgr --create --disk /dev/sda --part 1 --label "Alpine Linux" --loader /efi/boot/bootx64.efi

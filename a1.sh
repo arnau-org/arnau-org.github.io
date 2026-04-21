d32b6dd2e5ed3998856883eff2a2c490ef6cdda1
arnau-org <71732803+arnau-org@users.noreply.github.com>
2026-04-21 11:32:45 +0200

    Update a1.sh

--- a1.sh
+++ a1.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 set -e
 
-ISO_URL="https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/x86_64/alpine-extended-3.23.4-x86_64.iso"
+apk add --no-cache parted dosfstools e2fsprogs util-linux efibootmgr
 
-apk add --no-cache parted dosfstools e2fsprogs efibootmgr # uniso wget
+wipefs --all /dev/nvme0n1
 
 parted --script /dev/nvme0n1 mklabel gpt
-parted --script --align=optimal /dev/nvme0n1 mkpart ESP fat32 1MiB 513MiB
-parted --script --align=optimal /dev/nvme0n1 mkpart primary ext4 513MiB 8GiB  
-parted --script --align=optimal /dev/nvme0n1 mkpart primary ext4 8GiB 100%
+parted --script --align=optimal /dev/nvme0n1 mkpart ESP fat32 1MiB 2GiB
+parted --script --align=optimal /dev/nvme0n1 mkpart primary ext4 2GiB 6GiB  
+parted --script --align=optimal /dev/nvme0n1 mkpart primary ext4 6GiB 100%
 parted --script /dev/nvme0n1 set 1 esp on
 
 modprobe vfat
 modprobe ext4
 
 mkfs.vfat -F32 /dev/nvme0n1p1
-mkfs.ext4 -O ^has_journal,^64bit /dev/nvme0n1p2
+mkfs.ext4 -O ^has_journal /dev/nvme0n1p2
 mkfs.ext4 /dev/nvme0n1p3
 
-if wget -O /root/alpine.iso "$ISO_URL"; then
-  mount -t vfat /dev/nvme0n1p1 /mnt
-  cd /mnt
-  uniso < /root/alpine.iso
-  sync
-  cd ~ && umount /mnt
-  efibootmgr --create --disk /dev/nvme0n1 --part 1 --label "Alpine Linux" --loader \\EFI\\boot\\bootx64.efi
-fi
+mkdir -p /media/nvme0n1p1
+mount -t vfat /dev/nvme0n1p1 /media/nvme0n1p1
+cp -aT /media/sda /media/nvme0n1p1
+sync
+umount /media/nvme0n1p1
+
+# Ara no cal car ja està configurat de l'anterior vegada
+#efibootmgr --create --disk /dev/nvme0n1 --part 1 --label "Alpine Linux" --loader \\EFI\\boot\\bootx64.efi
